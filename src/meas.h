acc70b3dbae45f6d46157895f60ef8f965a7717e
Wen Wang <49297192+wenwwang@users.noreply.github.com>
2019-11-12 10:53:08 -0800

    remove gd and sign

--- src/meas.h
+++ src/meas.h
@@ -2,18 +2,18 @@
 
 #include "data.h"
 
-void measure_eqlt(const struct params *const restrict p, const int sign,
+void measure_eqlt(const struct params *const restrict p,// const int sign,
 		const double *const restrict gu,
-		const double *const restrict gd,
+		// const double *const restrict gd,
 		struct meas_eqlt *const restrict m);
 
-void measure_uneqlt(const struct params *const restrict p, const int sign,
+void measure_uneqlt(const struct params *const restrict p,// const int sign,
 		const double *const Gu0t,
 		const double *const Gutt,
 		const double *const Gut0,
-		const double *const Gd0t,
-		const double *const Gdtt,
-		const double *const Gdt0,
+		// const double *const Gd0t,
+		// const double *const Gdtt,
+		// const double *const Gdt0,
 		struct meas_uneqlt *const restrict m);
 
 /*
